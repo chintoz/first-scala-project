7da5707058483ed942980e1f679b0847b0278bfd
jjmena80 <chintoz_ltd@hotmail.com>
2017-12-17 02:33:35 +0100

    Added initial version MongoDB Scala connector

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.12.4"
 libraryDependencies += "commons-logging" % "commons-logging" % "1.1.1"
 
 libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.1.2"
+
+libraryDependencies += "org.mongodb" % "mongo-java-driver" % "2.10.1"
---
+++ src/main/scala/es/menasoft/mongodb/MongoClient.scala
@@ -0,0 +1,28 @@
+package es.menasoft.mongodb {
+
+  import com.mongodb.{DB, DBCollection, Mongo, DB => MongoDB}
+  import scala.collection.convert.Wrappers._
+
+  class MongoClient(val host: String, val port: Int) {
+    require(host != null, "You have to provide a host name")
+    private val underlying = new Mongo(host, port)
+    def this() = this("127.0.0.1", 27017)
+
+    def version = underlying.getVersion
+
+    def dropDB(name:String) = underlying.dropDatabase(name)
+
+    def createDB(name:String) = DB(underlying.getDB(name))
+
+    def db(name:String) = DB(underlying.getDB(name))
+  }
+
+  object DB {
+    def apply (underlying:MongoDB) = new DB(underlying)
+  }
+
+  class DB private (val underlying: MongoDB) {
+    def  collectionNames = for (name <- new JSetWrapper(underlying.getCollectionNames)) yield name
+  }
+
+}
